229960d231223323e957ec4566afd6872e67416a
Nemnomi <git@nemnomi.dev>
2025-10-05 14:01:46 +0900

    add flavored branch

---
+++ flavored/resonite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ $(echo "${RESONITE_HEADLESS_CODE}" | sha1sum - | head -c 40) != 'd1f3ba9ef7ebd9a57c5f4792ccdd690ab81173c3' ]]; then
+  echo 'Invalid headless code. Please get it from Resonite bot and set it as RESONITE_HEADLESS_CODE environment variable.' 1>&2
+  exit 1
+fi
+
+dotnet /opt/Resonite/Headless/Resonite.dll -Invisible -HeadlessConfig /var/lib/resonite/config/Config.json -Watchdog /var/lib/resonite/watchdog -Data /var/lib/resonite/data -Cache /var/lib/resonite/cache -Logs /var/lib/resonite/logs -LoadAssembly Libraries/ResoniteModLoader.dll "$@"
